5d8c42719ddd15e3252a80bb769ea07bfd3e45dc
sago35 <sago35@gmail.com>
2015-10-15 20:39:00 +0900

    不要部を削除

--- append_test.pl
+++ append_test.pl
@@ -26,27 +26,3 @@ printf "%s 4\n", $no;
 
 close $wfh;
 
-__END__
-# Makefile
-
-all: A B C D E F
-
-A:
-	perl -e "sleep 1"
-	perl asdf.pl A
-
-B:
-	perl -e "sleep 2"
-	perl asdf.pl B
-
-C:
-	perl asdf.pl C
-
-D:
-	perl asdf.pl D
-
-E:
-	perl asdf.pl E
-
-F:
-	perl asdf.pl F
